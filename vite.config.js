4a76c64ec12a600a0d6d7e5d602afc37e48da06b
mk-mojib <mdkhalidmojib2001@gmail.com>
2024-03-22 16:37:07 +0530

    again commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/COMPWALLAH_April-2024/",
+  base:"/COMPWALLAH_April-2024_1/",
 })
